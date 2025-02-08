40c22bff4056fbc16fd24247014bb9189e12d1d8
ThanuraSilva <thanurasilva1989@gmail.com>
2025-02-08 14:16:48 +0530

    Add table template to the BookConsole

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavB from './components/NavB';
+import { BookConsole } from './components/BookConsole';
 
 function App() {
   return (
     <>
       <NavB/>
+      <BookConsole/>
     </>
   );
 }
