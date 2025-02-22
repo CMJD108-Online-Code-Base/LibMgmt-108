df0e9df9410bb07d38c24cc97e1399c53e4bc93f
ThanuraSilva <thanurasilva1989@gmail.com>
2025-02-22 15:09:39 +0530

    Add routing

--- src/components/NavB.tsx
+++ src/components/NavB.tsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router'
 
 function NavB() {
   return (
@@ -8,10 +9,10 @@ function NavB() {
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Book</Nav.Link>
-            <Nav.Link href="#pricing">Lendings</Nav.Link>
-            <Nav.Link href="#features">Staff</Nav.Link>
-            <Nav.Link href="#pricing">Members</Nav.Link>
+            <Nav.Link as ={NavLink} to="/book">Book</Nav.Link>
+            <Nav.Link as ={NavLink} to="/lending">Lendings</Nav.Link>
+            <Nav.Link as ={NavLink} to="/staff">Staff</Nav.Link>
+            <Nav.Link as ={NavLink} to="/member">Members</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
---
+++ src/components/lendings/LendingConsole.tsx
@@ -0,0 +1,10 @@
+
+const LendingConsole = () =>{
+    return(
+        <>
+            <h1>Hello Lending Componet</h1>
+        </>
+    );
+}
+
+export default LendingConsole;
